00ebcd04608b4201e9932f5510ce6ec6d890e638
Olga <olia_tarasenko@inbox.ru>
2023-02-12 19:40:14 +0300

    первая задача

--- WhichValueIsMax/Program.cs
+++ WhichValueIsMax/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+// Пример:
+// а =5, b = 7 -> max = 7
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{Console.WriteLine("max");} 
+else
+Console.WriteLine("min");
